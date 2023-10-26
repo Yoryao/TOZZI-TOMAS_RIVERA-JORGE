f2ac8a7d2037fd09b88b9aacb689713a822558a5
jjrivera <jorgej.rivera@telefonica.com>
2023-10-25 22:12:23 -0300

    logger Working

---
+++ TOZZI-TOMAS_RIVERA-JORGE/src/com/backend/parcial/Main.java
@@ -0,0 +1,12 @@
+package com.backend.parcial;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    static Logger log = Logger.getLogger("Main.java");
+
+    public static void main(String[] args) {
+    
+
+    }
+}
